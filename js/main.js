c4d05ca82b23feac276f1c82e92e67aadf79723d
Wesley Werikis <wesleywerikis@outlook.com>
2025-06-30 15:51:00 -0300

    feat: display navigation menu based on user login and implement logou button.

--- js/main.js
+++ js/main.js
@@ -4,15 +4,42 @@ const anuncios = JSON.parse(localStorage.getItem("anuncios")) || [];
 const lista = document.getElementById("lista-anuncios");
 
 if (lista) {
-  anuncios.forEach(anuncio => {
-    const div = document.createElement("div");
-    div.className = "anuncio";
-    div.innerHTML = `
+    anuncios.forEach(anuncio => {
+        const div = document.createElement("div");
+        div.className = "anuncio";
+        div.innerHTML = `
       <h3>${anuncio.titulo}</h3>
       <p>${anuncio.descricao}</p>
       <p><strong>Categoria:</strong> ${anuncio.categoria}</p>
       <p><strong>Local:</strong> ${anuncio.local}</p>
     `;
-    lista.appendChild(div);
-  });
+        lista.appendChild(div);
+    });
+
+    const nav = document.getElementById("menuNavegacao");
+    const usuarioLogado = JSON.parse(localStorage.getItem("usuarioLogado"));
+
+    if (usuarioLogado) {
+        nav.innerHTML = `
+          <img src="https://www.gravatar.com/avatar?d=mp" width="30" style="border-radius: 50%; vertical-align: middle; margin-right: 8px;">
+          <span style="color: white; margin-right: 1rem;">${usuarioLogado.email}</span>
+          <a href="novo-anuncio.html" style="color: white; margin-right: 1rem;">Novo Anúncio</a>
+          <button onclick="logout()" style="padding: 5px 10px;">Sair</button>
+        `;
+    }
+    else {
+        // Mostra links padrão
+        nav.innerHTML = `
+    <a href="login.html">Login</a>
+    <a href="cadastro.html">Cadastrar-se</a>
+    <a href="novo-anuncio.html">Novo Anúncio</a>
+  `;
+    }
+
+    // Função de logout
+    function logout() {
+        localStorage.removeItem("usuarioLogado");
+        location.reload(); // recarrega a página
+    }
+
 }
